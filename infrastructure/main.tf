6e03770498c33250ddcad732e547f0e05362385c
Louis Van Steene <louis@lvs.me.uk>
2025-09-04 23:18:54 +0100

    fix: try another param

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,6 +6,7 @@ terraform {
     key                         = "terraform.tfstate"
     region                      = "us-east-1"
     endpoint                    = "https://nbg1.your-objectstorage.com"
+    skip_requesting_account_id  = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true

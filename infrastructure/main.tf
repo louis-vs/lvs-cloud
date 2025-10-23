666f009dcffe34c45e2650b71ccccd63a0faaa10
Louis Van Steene <louis@lvs.me.uk>
2025-10-23 20:12:46 +0100

    fix: terraform backend config

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,17 +2,14 @@ terraform {
   required_version = ">= 1.0"
 
   backend "s3" {
-    bucket = "lvs-cloud-terraform-state"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-    endpoints = {
-      s3 = "https://nbg1.your-objectstorage.com"
-    }
-    skip_requesting_account_id  = true
+    bucket                      = "lvs-cloud-terraform-state"
+    key                         = "terraform.tfstate"
+    region                      = "us-east-1"
+    endpoint                    = "https://nbg1.your-objectstorage.com"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    use_path_style              = true
+    force_path_style            = true
   }
 
   required_providers {
